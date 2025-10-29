bef4bc3fbc2f27102428afe247ae3ad9d0850be5
Lucas <ds.lucasilva@gmail.com>
2025-10-29 13:27:18 -0300

    First Commit

--- codes/src/Main.java
+++ codes/src/Main.java
@@ -4,68 +4,106 @@ public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    // função 6 = verifica o maior e o menor número
-    protected static void verificarMaiorMenor() {
-        System.out.print("Quantidade de números: ");
-        int qtdNumeros = scanner.nextInt();
-        int maior = Integer.MIN_VALUE;
-        int menor = Integer.MAX_VALUE;
-        for (int i = 0; i < qtdNumeros; i++) {
-            System.out.print("Número "+ (i+1)+ ": ");
-            int numero = scanner.nextInt();
-            if (numero >= maior) {
-                maior = numero;
+    // função 6 = inverter valores de um vetor
+    protected static void inverterArray() {
+        int[] vetor = { 0, 1, 2, 3, 4, 5, 6, 7 };
+        System.out.print("Vetor original: ");
+        for (int i=0; i<vetor.length; i++) {
+            System.out.print("("+ vetor[i]+ ")");
+        }
+        System.out.print("\nVetor invertido: ");
+        for (int i=vetor.length-1; i>=0; i--) {
+            System.out.print("("+ vetor[i]+ ")");
+        }
+    }
+
+    // função 5 = matriz de soma
+    protected static void matrizSoma() {
+        int[][] matriz = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Linha "+ (i+1)+ " | Coluna "+ (j+1)+ ": ");
+                int numero = scanner.nextInt();
+                matriz[i][j] = numero;
             }
-            if (numero <= menor) {
-                menor = numero;
-            }
+            System.out.println();
         }
-        System.out.println("Maior: "+ maior);
-        System.out.println("Menor: "+ menor);
-    }
-
-    // função 5 = calcular o fatorial de um número
-    protected static void calcularFatorial(int numero) {
-        int aux = 1;
-        for (int i = 1; i <= numero; i++) {
-            aux *= i;
-        }
-        System.out.println("Fatorial de \""+ numero+ "\" é \""+ aux+ "\".");
-    }
-
-    // função 4 = verificar se palavra é palíndromo
-    protected static String verificarPalindromo(String palavra) {
-        for (int i = 0, j = palavra.length() - 1; i < j; i++, j--) {
-            if (palavra.toLowerCase().charAt(i) != palavra.toLowerCase().charAt(j)) {
-                return "Palavra \""+ palavra+ "\" não é um palíndromo.";
-            }
-        }
-        return "Palavra \""+ palavra+ "\" é um palíndromo.";
-    }
-
-    // função 3 = gerar a tabuada do número informado
-    protected static void gerarTabuada(int numero) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(numero+ " x "+ i+ " = "+ (numero*i));
-        }
-    }
-
-    // função 2 = somar todos os números inseridos
-    protected static void somarNumerosInseridos() {
-        System.out.print("Quantidade de números: ");
-        int qtdNumeros = scanner.nextInt();
         int total = 0;
-        for (int i = 0; i < qtdNumeros; i++) {
-            System.out.print("Número "+ (i+1)+ ": ");
-            int numero = scanner.nextInt();
-            total += numero;
+        for (int i=0; i<matriz.length; i++) {
+            for (int j=0; j<matriz[i].length; j++) {
+                total += matriz[i][j];
+            }
         }
-        System.out.println("Total: "+ total);
+        System.out.println("Total valores da matriz: "+ total);
     }
 
-    // função 1 = verificar se é par ou ímpar
-    protected static String verificarParidade(int numero) {
-        return numero % 2 == 0 ? "Par" : "Ímpar";
+    // função 4 = verificar se o número é primo
+    protected static void verificarNumeroPrimo() {
+        System.out.print("Número: ");
+        int numero = scanner.nextInt();
+        for (int i = 2; i <= numero; i++) {
+            boolean isPrimo = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimo = false;
+                    break;
+                }
+            }
+            if (isPrimo) {
+                System.out.println("Número \""+ i+ "\" é primo.");
+            }
+        }
+    }
+
+    // função 3 = contagem total de caracteres sem espaçamento
+    protected static int contagemTotalCaracteres(String texto) {
+        return texto.replace(" ", "").length();
+    }
+
+    // função 2 = contagem de vogais e consoantes
+    protected static void contagemVogaisConsoantes() {
+        char[] vogais = { 'a', 'e', 'i', 'o', 'u' };
+        char[] consoantes = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+
+        System.out.print("Texto: ");
+        String texto = scanner.nextLine().toLowerCase();
+        String textoAlterado = texto.replace(" ", "");
+
+        int qtdVogais = 0;
+        int qtdConsoantes = 0;
+
+        for (int i = 0; i < textoAlterado.length(); i++) {
+            for (int j = 0; j < vogais.length; j++) {
+                if (textoAlterado.charAt(i) == vogais[j]) {
+                    qtdVogais++;
+                }
+            }
+            for (char c : consoantes) {
+                if (textoAlterado.charAt(i) == c) {
+                    qtdConsoantes++;
+                }
+            }
+        }
+        System.out.println("Quantidade de vogais: "+ qtdVogais);
+        System.out.println("Quantidade de consoantes: "+ qtdConsoantes);
+    }
+
+    // função 1 = ordenar valores em uma lista sem métodos prontos
+    protected static void ordenacaoVetor() {
+        int[] vetor = { 5, 1, 19, 42, 3, 7, 15 };
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length - 1; j++) {
+                if (vetor[j] > vetor[j+1]) {
+                    int aux = vetor[j];
+                    vetor[j] = vetor[j+1];
+                    vetor[j+1] = aux;
+                }
+            }
+        }
+        System.out.print("Vetor ordenado: ");
+        for (Integer v : vetor) {
+            System.out.printf("(%d)", v);
+        }
     }
 
     public static void main(String[] args) {
